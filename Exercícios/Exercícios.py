b7e2c914b6bc2c3e689acbc944f82a60c2fef865
camila <camila.vasconcelos.cf@gmail.com>
2023-06-26 18:16:19 -0300

    Atividades

---
+++ Exercícios/Exercícios.py
@@ -0,0 +1,67 @@
+#Atividade 1
+nome = "Camila";
+idade = "37";
+print()
+
+print(f"Olá, meu nome é {nome} e eu tenho {idade} anos.\n") ;
+
+nome = (nome);
+linguagem = "Python";
+print();
+
+print(f"Olá, eu sou {nome}. Estou Aprendendo {linguagem}.");
+print();
+
+#Atividade 2
+frase = "Curso Python ! ";
+print("Tamanho: ", len(frase));
+print();
+print("Frase: ", frase)
+print("Comprimento da frase: ", len(frase))
+
+print()
+
+#Atividade 3
+nome = "Camila";
+sobreNome = "Vasconcelos";
+print(f"Nome de completos: {nome} de {sobreNome}\n");
+
+#Atividade 4 
+print("3")
+
+frase = "Curso de Python"
+
+frasemaiuscula = frase.upper()
+print("Frase: ", frase)
+print("Frase em maiuscula: ", frasemaiuscula)
+
+print()
+
+#Atividade 5 
+frase = "Aulas de Segunda á Quinta - feira."
+palavras = frase.split()
+
+print("Frase: ", frase)
+print(palavras)
+
+print()
+
+#Atividade 6
+z = frase.replace("Python", "Estudando");
+print("Substituas as palavras: ", z);
+print();
+
+#Atividade 7 
+numero1 = 10;
+numero2 = 20;
+soma = numero1 + numero2;
+print("Soma: ", soma , "idade.");
+
+#Atividade 8 
+print("Exercício numero 8:")
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+
+resultado_da_multiplicação = num1 * num2
+
+print("Resultado: ", resultado_da_multiplicação)
